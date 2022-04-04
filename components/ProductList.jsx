4edd5e14ca80bca841249ed3d793bc6e38edd5aa
DVitsme <derrick.valentine@gmail.com>
2022-04-04 13:19:19 -0400

    Display Stores Skelton

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -38,7 +38,12 @@ export default function ProductList({ products = [] }) {
                 </div>
               </div>
               <div className="mt-6">
-                <Link href={`/coffee-store/${product.title}`}>
+                <Link
+                  href={{
+                    pathname: `/coffee-store/${product.id}`,
+                    query: { title: product.title }
+                  }}
+                >
                   <a className="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200">
                     Add to bag<span className="sr-only">, {product.title}</span>
                   </a>
